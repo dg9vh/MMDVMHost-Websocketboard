cb27cfbbd80a86ec26cb61b72cf8f92e416db4fe
Kim - DG9VH <dg9vh@users.noreply.github.com>
2020-11-03 15:00:35 +0100

    Bugfixing

--- html/js/functions.js
+++ html/js/functions.js
@@ -99,41 +99,62 @@ function getLastHeard(document, event) {
 // 01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
 // M: 2020-11-01 21:33:27.454 YSF, received network data from DG2MAS     to DG-ID 0 at DG2MAS
 // M: 2020-11-01 21:33:35.025 YSF, received network end of transmission from DG2MAS     to DG-ID 0, 7.7 seconds, 0% packet loss, BER: 0.0%
+	if (event.data.indexOf("Talker Alias") < 0 && event.data.indexOf("Downlink Activate") < 9) {
 	$(document).ready(function() {
-		var rowIndexes = [];
+		var rowIndexes = [],
+		timestamp = getTimestamp(event.data),
+		mode = getMode(event.data),
+		callsign = getCallsign(event.data),
+		target = getTarget(event.data),
+		source = getSource(event.data),
+		duration = getDuration(event.data),
+		loss = getLoss(event.data),
+		ber = getBER(event.data),
+		addToQSO = getAddToQSO(event.data);
+		if (mode == "POCSAG") {
+			callsign = "POCSAG";
+			target = "";
+			source = "";
+			duration = "";
+			loss = "";
+			ber = "";
+			addToQSO = "";
+		}
 		t_lh.rows( function ( idx, data, node ) {
-			if(data[2] === getCallsign(event.data)){
+			if(data[2] === callsign){
 				rowIndexes.push(idx);
 			}
 			return false;
 		});
+
 		if (rowIndexes[0]) {
 			newData = [
-				getTimestamp(event.data),
-				getMode(event.data),
-				getCallsign(event.data),
-				getTarget(event.data),
-				getSource(event.data),
-				getDuration(event.data),
-				getLoss(event.data),
-				getBER(event.data),
-				getAddToQSO(event.data)
+				timestamp,
+				mode,
+				callsign,
+				target,
+				source,
+				duration,
+				loss,
+				ber,
+				addToQSO
 			]
 			t_lh.row(rowIndexes[0]).data( newData ).draw();
 		} else {
 			t_lh.row.add( [
-				getTimestamp(event.data),
-				getMode(event.data),
-				getCallsign(event.data),
-				getTarget(event.data),
-				getSource(event.data),
-				getDuration(event.data),
-				getLoss(event.data),
-				getBER(event.data),
-				getAddToQSO(event.data)
+				timestamp,
+				mode,
+				callsign,
+				target,
+				source,
+				duration,
+				loss,
+				ber,
+				addToQSO
 			] ).draw();
 		}
 	});
+	}
 }
 
 function getLocalHeard(document, event) {
