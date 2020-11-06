45060d5b0cbe0b1efe8e430470de528d7ce21856
Kim - DG9VH <dg9vh@users.noreply.github.com>
2020-11-06 22:26:49 +0100

    Update config.js

--- html/js/config.js
+++ html/js/config.js
@@ -1,9 +1,9 @@
-# 1 = show link to QRZ.com, 0 = off
+// 1 = show link to QRZ.com, 0 = off
 var qrz = 1;
 
-# 1 = enable debug in javascript-console, 0 = 0ff
-var debug = 0;
+// 1 = enable debug in javascript-console, 0 = 0ff
+var debug = 1;
 
-# Set messagecounters for different badge-colors
+// Set messagecounters for different badge-colors
 var warnlevel = 200;
 var emergencylevel = 500;
